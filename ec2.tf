4740191695ee41c0e2ad36d557be308fbacdd82c
kennedyviegas <kennedy.viegas42@gmail.com>
2024-08-05 17:28:42 -0300

    alterações feitas

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,9 @@ resource "aws_instance" "app_server" {
   ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
   key_name = "devops"
-  
+  #security_groups = [ "allow_ssh","allow_http","allow_egress" ]
+  security_groups = [aws_security_group.example.name]
+
 
   tags = {
     Name = "app_server"
